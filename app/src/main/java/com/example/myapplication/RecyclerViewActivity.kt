e384c493c828b09381b54061886ff3493dd74d7e
Grzegorz Oleksiuk <goleksiukk@gmail.com>
2019-05-20 13:05:49 +0200

    Warnings fix

--- app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 
@@ -12,8 +13,8 @@ class RecyclerViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recycler_view)
 
         val bundle: Bundle? = intent.extras
-        val calculationList: ArrayList<Calculation> = bundle!!.getParcelableArrayList("EXTRA_LIST")
-        calculationList.reverse()
+        val calculationList: ArrayList<Calculation>? = bundle!!.getParcelableArrayList("EXTRA_LIST")
+        calculationList!!.reverse()
 
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.adapter = ItemsAdapter(calculationList)
